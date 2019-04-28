4f1bed2fc84a57a1b30569ee6a9f014ea016862c
student16 <student16@pcoip.ki.agh.edu.pl>
2019-04-28 15:48:05 +0200

    update

--- src/main/java/com/company/enroller/security/WebSecurity.java
+++ src/main/java/com/company/enroller/security/WebSecurity.java
@@ -1,9 +1,12 @@
 package com.company.enroller.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -14,5 +17,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                .anyRequest().permitAll() // Pozwala wysłać zapytania do serwera domyślnie zabezpieczone 
                .and()
                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+   }
+   @Autowired
+   ParticipantProvider participantProvider;
+   
+   @Autowired
+   PasswordEncoder passwordEncoder;
+   
+   @Override
+   public void configure(AuthenticationManagerBuilder auth) throws Exception {
+      auth.userDetailsService(participantProvider).passwordEncoder(passwordEncoder);
    }
 }
